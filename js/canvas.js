f4fc1a7ccdb57b5885577a36fd0f7eed60ede59d
rlangille2 <rlangille2@gmail.com>
2018-10-12 19:31:54 -0300

    Added background

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,13 @@
 
 export class Canvas {
     constructor(_width, _height){
+        this.div = document.getElementById("grid1");
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = _width;
         this.canvas.height = _height;
 
-        document.body.appendChild(this.canvas);
+        this.div.appendChild(this.canvas);
     }
 
     getCtx(){
--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import {GameLoop} from './gameLoop.js';
 import {Asset} from './asset.js';
 import {AssetLoader} from './assetLoader.js';
 
-var canvas = new Canvas(600,600);
+var canvas = new Canvas(1600,1400);
 var ctx = canvas.getCtx();
 var gameLoop = new GameLoop(ctx);
+var bgForest = new Asset("img/bg-1.png",0,0, true);
 var rita = new Asset("img/rita.png", 0, 0, true);
 var assetLoader = new AssetLoader();
+    assetLoader.render(bgForest);
     assetLoader.render(rita);
 
 gameLoop.setAssets(assetLoader.assets);
